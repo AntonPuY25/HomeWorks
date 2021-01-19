8f529f29fac8f655e3321918fe37020c0bacde9e
AntonPuY25 <puy25@bk.ru>
2021-01-19 14:14:16 +0300

    HomeWork6

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import s from "./App.module.css";
-import HW5 from "../../../p2-homeworks/h5/HW5";
+import HW6 from "../../../p2-homeworks/h6/HW6";
 
 function App() {
     return (
@@ -8,7 +8,7 @@ function App() {
             <div>react homeworks:</div>
 
 
-            <HW5/>
+            <HW6/>
 
         </div>
     );
